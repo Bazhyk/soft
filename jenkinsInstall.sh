d4335961596161d7c32521ed35b201a9d9a06d1b
Bazhyk <dbozhuha@gmail.com>
2021-03-10 20:11:52 +0200

    Terraform

---
+++ jenkinsInstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins openjdk-11-jdk -y
+echo "Password to jenkins "
+sudo cat  /var/lib/jenkins/secrets/initialAdminPassword
+sudo reboot
