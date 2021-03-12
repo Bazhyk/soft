67ecb7a31c4e2b75523317dd804a5697877880d5
Bazhyk <dbozhuha@gmail.com>
2021-03-12 13:06:50 +0200

    jenkinsInstall.sh

--- jenkinsInstall.sh
+++ jenkinsInstall.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
+apt-get install  openjdk-11-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins openjdk-11-jdk -y
+apt-get update -y
+apt-get install jenkins -y
 echo "Password to jenkins "
-sudo cat  /var/lib/jenkins/secrets/initialAdminPassword
-sudo reboot
+cat  /var/lib/jenkins/secrets/initialAdminPassword
